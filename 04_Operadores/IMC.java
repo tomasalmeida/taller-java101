42a6fe0d472427043a24ebd64402795db3f1c8ea
Tomás Dias Almeida <tomas.dias@netcentric.biz>
2017-10-19 18:27:39 +0200

    imc class

---
+++ 04_Operadores/IMC.java
@@ -0,0 +1,11 @@
+public class IMC {
+
+	public static void main(String[] args){
+
+		short peso = 81;
+		double altura = 1.81;
+		double imc = peso / (altura * altura);
+
+		System.out.println("Mi imc es " + imc);
+	}
+}
